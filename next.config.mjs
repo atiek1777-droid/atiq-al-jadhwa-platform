18233e5256296c1f3e5fb2abb82a58ea470548d9
atqqk <atiek1777@gmail.com>
2026-09-13 05:00:54 +0300

    Fix next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: false }
+  eslint: { ignoreDuringBuilds: true }
 };
 
 export default nextConfig;
